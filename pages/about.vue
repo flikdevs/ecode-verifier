f9e63d128e53eaebe4931f7c17c90a53e2492fde
Alisher Usmonov <83527629+Alisher-Usmonov@users.noreply.github.com>
2022-12-23 17:49:07 +0500

    feat: blank

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useHead({
+  title: "About",
+});
+</script>
+
+<template>
+  <div>about</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>wOOOw</h1>
+    <NuxtLink to="/about">About</NuxtLink>
+  </div>
+</template>
